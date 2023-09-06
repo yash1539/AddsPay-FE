a65645e41e4187a995199a99c10da30291981af1
Yash <Yash.singh@internlabs.com>
2023-09-06 19:59:56 +0530

    added google sigin

--- src/components/comman/Login.js
+++ src/components/comman/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { signInWithGoogle } from '../../firebase'
 
 const Login = () => {
   return (
@@ -59,10 +60,16 @@ const Login = () => {
           <button
             type="submit"
             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
+           >
             Sign in
           </button>
         </div>
+        <button
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+           onClick={signInWithGoogle}>
+            Sign in with Google
+          </button>
       </form>
 
       <p className="mt-10 text-center text-sm text-gray-500">
